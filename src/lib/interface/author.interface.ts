41cd097fbc6955c101f098c78b5a59a66bb59ed3
Lee Norris <lee.norris@arcadia.io>
2021-03-18 19:33:09 -0400

    fix: author

--- src/lib/interface/author.interface.ts
+++ src/lib/interface/author.interface.ts
@@ -1,9 +1,8 @@
 import { IIdentifiable } from '@onivoro/isomorphic-crud-provider';
-import { IProsable } from 'src';
 import { IArticle } from './article.interface';
 import { IRegion } from './region.interface';
 
-export interface IAuthor extends IIdentifiable, IRegion, IProsable {
+export interface IAuthor extends IIdentifiable, IRegion {
     firstName: string,
     lastName: string,
     linkedInProfile?: string
