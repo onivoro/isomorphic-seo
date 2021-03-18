4bca098e05981b7637edc6984c76ae90f0b8fc36
Lee Norris <lee.norris@arcadia.io>
2021-03-18 19:26:18 -0400

    chore: enhance author

--- src/lib/interface/author.interface.ts
+++ src/lib/interface/author.interface.ts
@@ -1,12 +1,12 @@
 import { IIdentifiable } from '@onivoro/isomorphic-crud-provider';
+import { IProsable, ITaggable } from 'src';
 import { IArticle } from './article.interface';
+import { IRegion } from './region.interface';
 
-export interface IAuthor extends IIdentifiable {
+export interface IAuthor extends IIdentifiable, IRegion, IProsable, ITaggable {
     firstName: string,
     lastName: string,
     linkedInProfile?: string
     articles?: IArticle[];
     email?: string;
-    title?: string;
-    description?: string;
 }
