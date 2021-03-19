b7014d93d6fc806a485964e6934e02fd8f78b602
Lee Norris <lee.norris@arcadia.io>
2021-03-18 20:35:49 -0400

    chore: rm publication; add author 2 article

--- src/lib/interface/article.interface.ts
+++ src/lib/interface/article.interface.ts
@@ -1,6 +1,8 @@
 import { IIdentifiable } from '@onivoro/isomorphic-crud-provider'
+import { IAuthor } from './author.interface';
 import { IProsable } from "./prosable.interface";
 import { IRegion } from './region.interface';
 
 export interface IArticle extends IIdentifiable, IRegion, IProsable {
+    authors?: IAuthor[];
 }
--- src/lib/interface/publication.interface.ts
+++
@@ -1,6 +0,0 @@
-import { IArticle } from "./article.interface";
-import { IAuthor } from "./author.interface";
-
-export interface IPublication extends IArticle {
-    authors: IAuthor[];
-}
